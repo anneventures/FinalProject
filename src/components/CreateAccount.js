d6298dbfc872da8b1ecd06da0678213487f47536
Annecodes <info.annesimon@gmail.com>
2018-10-22 21:00:37 -0400

    added authentication files

---
+++ src/components/CreateAccount.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import { Container, Grid } from 'semantic-ui-react'
+
+import CreateAccountForm from './CreateAccountForm'
+
+class CreateAccount extends Component {
+	render() {
+		return (
+			<Container className='create_account' style={{textAlign: 'center'}}>
+
+				<Grid style={{marginTop:60}}>
+					<Grid.Column textAlign='right' width={16}>
+						<Link to="/">Sign in</Link>
+					</Grid.Column>
+				</Grid>
+
+			<CreateAccountForm />
+		</Container>
+		)
+	}
+}
+
+// react-redux
+function mapStateToProps ( {user} ) {
+	return {
+		user
+	}
+}
+
+export default withRouter ( connect( mapStateToProps )(CreateAccount))
--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// react-router-dom
+import { BrowserRouter } from 'react-router-dom'
+
+// redux
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+
+// css
+import 'semantic-ui-css/semantic.min.css';
+
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
