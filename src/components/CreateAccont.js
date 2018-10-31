a7177c446c1492979f99d6ec665ab34f171e22e7
Nero_Sen <niravsen11@gmail.com>
2018-10-31 18:45:28 -0400

    css for Login Page

--- src/components/CreateAccont.js
+++ src/components/CreateAccont.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import '../components/font.css'
+import '../components/CreateAccount.css'
+ 
+
 
 // semantic-ui
 import { Container, Grid } from 'semantic-ui-react'
 
 import CreateAccontForm from './CreateAccontForm'
+
 
 class CreateAccont extends Component {
 
@@ -14,7 +19,7 @@ class CreateAccont extends Component {
 
     return(
 
-      <Container className='create_acount' style={{textAlign: 'center'}}>
+      <Container className="create" style={{textAlign: 'center'}}>
        <h1>BUDGET+</h1>
 
         <CreateAccontForm />
